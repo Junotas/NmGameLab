1a597e890ff5320c98f1cc0153ef288de4bd75fc
Ludvig <l.fendert@gmail.com>
2025-02-06 11:00:19 +0100

    created the extended classes of the human player and npc(nonPlayableChar)

--- src/main/java/com/uppsala/player/HumanPlayer.java
+++ src/main/java/com/uppsala/player/HumanPlayer.java
@@ -1,4 +1,29 @@
 package com.uppsala.player;
 
-public class HumanPlayer {
+import java.util.Scanner;
+
+public class HumanPlayer extends Player {
+    private Scanner scanner;
+
+    public HumanPlayer(String name) {
+        super(name);
+        scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public int takeSticks(int sticksRemaining) {
+        int max = sticksRemaining / 2;
+        if (max < 1) {
+            max = 1;
+        }
+        System.out.println("\n" + name + ", det är din tur.");
+        System.out.println("Stickor kvar: " + sticksRemaining);
+        System.out.print("Ange antal stickor att ta (min 1, max " + max + "): ");
+        int taken = scanner.nextInt();
+        while (taken < 1 || taken > max) {
+            System.out.print("Ogiltigt antal. Ange ett tal mellan 1 och " + max + ": ");
+            taken = scanner.nextInt();
+        }
+        return taken;
+    }
 }
--- src/main/java/com/uppsala/player/NPC.java
+++ src/main/java/com/uppsala/player/NPC.java
@@ -1,4 +1,19 @@
 package com.uppsala.player;
 
-public class NPC {
+public class NPC extends Player {
+    public NPC(String name) {
+        super(name);
+    }
+
+    @Override
+    public int takeSticks(int sticksRemaining) {
+        int max = sticksRemaining / 2;
+        if (max < 1) {
+            max = 1;
+        }
+        // Enkel strategi: NPC tar alltid 1 sticka.
+        int taken = 1;
+        System.out.println("\n" + name + " (NPC) tar " + taken + " sticka(n).");
+        return taken;
+    }
 }
