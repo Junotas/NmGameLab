ba400187c4d9ce91ece6faf91798b07c1eec4949
Ludvig <l.fendert@gmail.com>
2025-02-06 11:04:24 +0100

    NmGame and Main setup

--- src/main/java/com/uppsala/Main.java
+++ src/main/java/com/uppsala/Main.java
@@ -1,7 +1,26 @@
 package com.uppsala;
+import com.uppsala.player.HumanPlayer;
+import com.uppsala.player.NPC;
+import com.uppsala.player.Player;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length < 1) {
+            System.out.println("Ange antal stickor vid start som ett argument.");
+            System.exit(1);
+        }
+        int sticks = Integer.parseInt(args[0]);
+        if (sticks < 1) {
+            System.out.println("Antalet stickor måste vara minst 1.");
+            System.exit(1);
+        }
+
+        // Skapa spelare: en mänsklig spelare och en NPC.
+        Player human = new HumanPlayer("Människan");
+        Player npc = new NPC("NPC");
+
+        // Skapa och starta spelet.
+        NmGame game = new NmGame(human, npc, sticks);
+        game.play();
     }
 }
--- src/main/java/com/uppsala/NmGame.java
+++ src/main/java/com/uppsala/NmGame.java
@@ -1,4 +1,37 @@
 package com.uppsala;
+import com.uppsala.player.Player;
 
 public class NmGame {
+    private final Player player1;
+    private final Player player2;
+    private int sticks;
+
+    public NmGame(Player player1, Player player2, int sticks) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.sticks = sticks;
+    }
+
+    public void play() {
+        Player current = player1;
+        Player other = player2;
+
+        while (sticks > 1) {
+            int max = sticks / 2;
+            int taken = current.takeSticks(sticks);
+            if (taken < 1 || taken > max) {
+                System.out.println("Ogiltigt drag av " + current.getName() + ". " + other.getName() + " vinner spelet!");
+                return;
+            }
+            sticks -= taken;
+            System.out.println(current.getName() + " tar " + taken + " sticka(n). Stickor kvar: " + sticks);
+            // Byt spelare
+            Player temp = current;
+            current = other;
+            other = temp;
+        }
+
+        System.out.println("\nEndast 1 stick kvar. " + current.getName() + " kan inte göra ett giltigt drag.");
+        System.out.println(other.getName() + " vinner spelet!");
+    }
 }
