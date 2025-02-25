b951abb40cfbdb54d7403c4ef69ff67e1cfe2cee
Ludvig <l.fendert@gmail.com>
2025-02-25 11:53:24 +0100

    removed comments

--- src/main/java/com/uppsala/player/Player.java
+++ src/main/java/com/uppsala/player/Player.java
@@ -7,11 +7,6 @@ public abstract class Player {
         this.name = name;
     }
 
-    /**
-     * Metod för att ta ett drag.
-     * @param sticksRemaining antalet stickor kvar i högen
-     * @return antalet stickor att ta (mellan 1 och floor(sticksRemaining/2))
-     */
     public abstract int takeSticks(int sticksRemaining);
 
     public String getName() {
