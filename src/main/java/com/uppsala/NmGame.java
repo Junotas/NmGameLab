c2fa6ad7a55ab9378749774dc1bf203779c0bcb8
Ludvig <l.fendert@gmail.com>
2025-02-25 11:53:05 +0100

    Constants for the NmGame.java class

--- src/main/java/com/uppsala/NmGame.java
+++ src/main/java/com/uppsala/NmGame.java
@@ -6,6 +6,20 @@ public class NmGame {
     private final Player player2;
     private int sticks;
 
+    // Konstanter
+    private static final String GAME_START_PREFIX = "Spelet börjar med ";
+    private static final String GAME_START_SUFFIX = " stickor på bordet.\n";
+    private static final String INVALID_MOVE_PREFIX = "Ogiltigt drag av ";
+    private static final String WINNER_SUFFIX = " vinner spelet!";
+    private static final String SINGULAR_STICK = " sticka";
+    private static final String PLURAL_STICKS = " stickor";
+    private static final String TAKES_PREFIX = " tar ";
+    private static final String TAKES_SUFFIX = ". ";
+    private static final String REMAINING_PREFIX = "Det finns nu ";
+    private static final String REMAINING_SUFFIX = " kvar.";
+    private static final String LAST_STICK_PREFIX = "\nDet ligger bara 1 sticka kvar på bordet. ";
+    private static final String NO_VALID_MOVE_SUFFIX = " kan inte göra ett giltigt drag.";
+
     public NmGame(Player player1, Player player2, int sticks) {
         this.player1 = player1;
         this.player2 = player2;
@@ -16,23 +30,23 @@ public class NmGame {
         Player current = player1;
         Player other = player2;
 
-        // Show initial state
-        System.out.println("Spelet börjar med " + sticks + " stickor på bordet.\n");
+        // Visa startmeddelande
+        System.out.println(GAME_START_PREFIX + sticks + GAME_START_SUFFIX);
 
         while (sticks > 1) {
             int max = sticks / 2;
             int taken = current.takeSticks(sticks);
             if (taken < 1 || taken > max) {
-                System.out.println("Ogiltigt drag av " + current.getName() + ". " + other.getName() + " vinner spelet!");
+                System.out.println(INVALID_MOVE_PREFIX + current.getName() + ". " + other.getName() + WINNER_SUFFIX);
                 return;
             }
             sticks -= taken;
 
-            // Plural / singular handling
-            String stickWord = (taken == 1) ? " sticka" : " stickor";
-            String remainWord = (sticks == 1) ? " sticka" : " stickor";
-            System.out.println(current.getName() + " tar " + taken + stickWord + ". "
-                    + "Det finns nu " + sticks + remainWord + " kvar.");
+            // Hantera singular och plural
+            String stickWord = (taken == 1) ? SINGULAR_STICK : PLURAL_STICKS;
+            String remainWord = (sticks == 1) ? SINGULAR_STICK : PLURAL_STICKS;
+            System.out.println(current.getName() + TAKES_PREFIX + taken + stickWord + TAKES_SUFFIX
+                    + REMAINING_PREFIX + sticks + remainWord + REMAINING_SUFFIX);
 
             // Byt spelare
             Player temp = current;
@@ -40,9 +54,8 @@ public class NmGame {
             other = temp;
         }
 
-        // If there's only 1 stick left, current can't make a valid move
-        System.out.println("\nDet ligger bara 1 sticka kvar på bordet. "
-                + current.getName() + " kan inte göra ett giltigt drag.");
-        System.out.println(other.getName() + " vinner spelet!");
+        // Om det bara finns 1 sticka kvar, kan current inte göra ett giltigt drag
+        System.out.println(LAST_STICK_PREFIX + current.getName() + NO_VALID_MOVE_SUFFIX);
+        System.out.println(other.getName() + WINNER_SUFFIX);
     }
 }
